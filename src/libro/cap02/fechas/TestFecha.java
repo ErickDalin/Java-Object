cb5dbfccc663c05c4d4f29357869d4886c093be8
DALIN <DALIN@DESKTOP-FTH2U89>
2025-07-05 19:11:12 -0500

    Sobrescritura de métodos toString and equals

---
+++ src/libro/cap02/fechas/TestFecha.java
@@ -0,0 +1,28 @@
+
+package libro.cap02.fechas;
+
+
+public class TestFecha {
+
+    
+    public static void main(String[] args) {
+        
+        Fecha f = new Fecha();
+        f.setDia(12);
+        f.setMes(12);
+        f.setAnio(2012);
+        
+        System.out.println("Dia "+f.getDia());
+        System.out.println("Mes "+f.getMes());
+        System.out.println("Anio "+f.getAnio());
+        
+        
+        Fecha f1 = new Fecha(12,12,2012);
+        
+        System.out.println("Fecha "+f);
+        System.out.println("Fecha "+f);
+        
+        System.out.println("Son iguales? "+f.equals(f1));
+    }
+    
+}
